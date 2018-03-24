4274c3d0fbcba57f681f229c092ba6f72dcc620d
phR0ze <prc451@gmail.com>
2018-03-23 21:34:23 -0600

    Fix for optparse

--- lib/nub/commander.rb
+++ lib/nub/commander.rb
@@ -19,7 +19,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-require 'options'
+require 'optparse'
 require 'colorize'
 
 # Command option class provides a way to encapsulate a command with additional
